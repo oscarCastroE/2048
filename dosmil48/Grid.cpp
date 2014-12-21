49884478cc4bb2509f801ea67d4a6d48372c7bd9
ivannia-alvarado <mana1893@hotmail.com>
2014-12-21 13:46:44 -0600

    Proyectito de Netbeans

---
+++ dosmil48/Grid.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Grid.cpp
+ * Author: Iva
+ * 
+ * Created on 21 de diciembre de 2014, 01:00 PM
+ */
+
+#include "Grid.h"
+
+Grid::Grid() {
+}
+
+Grid::Grid(const Grid& orig) {
+}
+
+Grid::~Grid() {
+}
+
---
+++ dosmil48/Grid.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Grid.h
+ * Author: Iva
+ *
+ * Created on 21 de diciembre de 2014, 01:00 PM
+ */
+
+#ifndef GRID_H
+#define	GRID_H
+class Grid {
+public:
+	Grid();
+	Grid(const Grid& orig);
+	virtual ~Grid();
+private:
+
+};
+
+#endif	/* GRID_H */
+
